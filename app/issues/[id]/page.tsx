50ce1df399f3b35256995b97bf3a1ae19513c3cc
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-27 13:29:01 -0400

    Add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,23 +1,29 @@
 import prisma from "@/prisma/client";
+import { Grid, Box, Flex } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 import { string } from "zod";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
 }
 const IssueDetailPage = async ({ params: { id } }: Props) => {
-  if (typeof id !== "number") notFound();
   const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
   if (!issue) notFound();
 
   return (
-    <div>
-      <p>{issue.title}</p>
-      <p>{issue.description} </p>
-      <p> {issue.status}</p>
-      <p> {issue.createdAt.toDateString()}</p>
-    </div>
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+    <Box className="md:col-span-4">
+     <IssueDetails issue={issue}/>
+    </Box>
+     {/* <Box>
+       <Flex direction={'column'} gap='4'>
+       <EditIssueButton issueId={issue.id} />
+       <DeleteIssueButton issueId={issue.id}/>
+       </Flex>
+     </Box> */}
+   </Grid>
   );
 };
 
