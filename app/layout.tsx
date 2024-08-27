1ae671b176eddca6847a193f89baed09d6eab98e
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-26 21:47:28 -0400

    Customize Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "@radix-ui/themes/styles.css";
+import './theme-config.css'
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme, ThemePanel } from "@radix-ui/themes";
 import Navbar from "./Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body >
+        <Theme radius="large">
+          {" "}
           <Navbar />
           <main>{children}</main>
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
