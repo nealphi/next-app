4d268775979839ecfc5fd08907b90dbe0db4fc0e
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-26 20:30:05 -0400

    Building an API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import { z } from "zod";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required"),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
