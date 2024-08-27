9c262f2eb9bf8bbcd2c2f95ffac8566e526c820b
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-27 13:17:04 -0400

    Show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import { Flex, Card, Box } from "@radix-ui/themes";
+
+const LoadingIssueDetailPage = () => {
+  return (
+    <Box className="max-w-xl">
+     Loading...
+    </Box>
+  );
+};
+
+export default LoadingIssueDetailPage;
---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,24 @@
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+import { string } from "zod";
+
+interface Props {
+  params: { id: string };
+}
+const IssueDetailPage = async ({ params: { id } }: Props) => {
+  if (typeof id !== "number") notFound();
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description} </p>
+      <p> {issue.status}</p>
+      <p> {issue.createdAt.toDateString()}</p>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
