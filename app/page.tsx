7b97d7e5b753e6cd3747175cd39ae43996979da6
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-29 16:34:28 -0400

    Build the layout of the Pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return (
-  <div></div>
-  );
+  return <Pagination itemCount={100} pageSize={10} currentPage={2} />;
 }
