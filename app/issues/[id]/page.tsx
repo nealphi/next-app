a6998d1aee4644141deab74c21b658104e4cba68
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-28 22:44:57 -0400

    Build the AssigneeSelect component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -8,6 +8,7 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
+import AssigneeSelect from "./AssigneeSelect";
 
 interface Props {
   params: { id: string };
@@ -24,6 +25,7 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
       </Box>
      {session && <Box>
         <Flex direction={"column"} gap="4">
+          <AssigneeSelect/>
           <EditIssueButton issueId={issue.id} />
           <DeleteIssueButton issueId={issue.id} />
         </Flex>
