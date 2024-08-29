35f57f05183c6d004c4e2ad27d9bee130bc92449
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-28 23:50:20 -0400

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { issueSchema, patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -15,9 +15,18 @@ export async function PATCH(request: NextRequest, { params: { id } }: Props) {
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user!" }, { status: 401 });
+  }
+
   const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
 
   if (!issue)
@@ -25,7 +34,7 @@ export async function PATCH(request: NextRequest, { params: { id } }: Props) {
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: { title: body.title, description: body.description, assignedToUserId: body.assignedToUserId },
   });
   return NextResponse.json(updatedIssue, { status: 201 });
 }
@@ -33,7 +42,7 @@ export async function PATCH(request: NextRequest, { params: { id } }: Props) {
 export async function DELETE(request: NextRequest, { params: { id } }: Props) {
   const session = await getServerSession(authOptions);
   if (!session) return NextResponse.json({}, { status: 401 });
-  
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
   });
