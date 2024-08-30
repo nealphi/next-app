96d8fafaf65c3d09015f16d574e48e350c2a36bc
Negin Alphi <negin.alipanahi@gmail.com>
2024-08-29 23:07:27 -0400

    Build the IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
+import { Status } from "@prisma/client";
 
 interface Props {
   searchParams: {
-    page: string
-  }
+    page: string;
+  };
 }
 
-export default function Home({searchParams: {page}}: Props) {
-  return <div>
-    <LatestIssues/>
-  </div>
+export default async function Home({ searchParams: { page } }: Props) {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return (
+    <div>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+    </div>
+  );
 }
